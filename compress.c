27f2e8230f4701256259efa4562fa40c1de3ddfb
shaharBabkoff <155917341+shaharBabkoff@users.noreply.github.com>
2024-03-01 14:23:28 +0200

    Update compress.c

--- compress.c
+++ compress.c
@@ -3,6 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Define constants for binary representations
+const char *BINARY_REPRESENTATIONS[] = {
+    "1111", // 0
+    "1110", // 1
+    "1101", // 2
+    "1100", // 3
+    "1011", // 4
+    "1010", // 5
+    "1001", // 6
+    "1000", // 7
+    "0111", // 8
+    "0110"  // 9
+};
 
 void compress(const char *filename) {
     // Open the input file
