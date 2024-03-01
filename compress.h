9033aef773dffe6a7ec599c455ac8b95d8f7768a
shaharBabkoff <155917341+shaharBabkoff@users.noreply.github.com>
2024-03-01 14:22:45 +0200

    Update compress.h

--- compress.h
+++ compress.h
@@ -1,19 +1,7 @@
 #ifndef COMPRESS_H
 #define COMPRESS_H
 
-// Define constants for binary representations
-const char *BINARY_REPRESENTATIONS[] = {
-    "1111", // 0
-    "1110", // 1
-    "1101", // 2
-    "1100", // 3
-    "1011", // 4
-    "1010", // 5
-    "1001", // 6
-    "1000", // 7
-    "0111", // 8
-    "0110"  // 9
-};
+extern const char *BINARY_REPRESENTATIONS[];
 
 void compress(const char *filename);
 void decompress(const char *filename);
